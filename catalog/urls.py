2066b2e4da5945b83697aec4cbb3fd8675825326
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-10-03 20:24:39 +0300

    fixed errors

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,6 @@ import catalog.views as catalog_views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', catalog_views.HomeView.as_view(), name='home'),
-    path('products/', catalog_views.products_list, name='products_list'),
+    path('', catalog_views.products_list, name='products_list'),
     path('products/<int:pk>/', catalog_views.product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
 
 from .models import Product
-
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        latest_products = Product.objects.order_by('-created_at')[:5]
-        print(latest_products)  # Вывод в консоль
-        context['latest_products'] = latest_products
-        return context
 
 
 def products_list(request):
