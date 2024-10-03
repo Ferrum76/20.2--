1688044d896055bfe273a4c0f2c0a8ea33caf427
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-10-03 21:44:02 +0300

    created for admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+@admin.register(Blog)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "body", "image", "views_count", "is_published")
+    list_filter = ("title", "created_at", "is_published")
+    search_fields = ("title",)
